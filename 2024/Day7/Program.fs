c462be8e5398362be394e3cd79e6b1c0c63c4ebc
Jamie McNair <8563840+jrmcnair@users.noreply.github.com>
2024-12-07 00:01:52 -0500

    Day 7: Bridge Repair

---
+++ 2024/Day7/Program.fs
@@ -0,0 +1,48 @@
+﻿open System.IO
+open System.Text.RegularExpressions
+
+[<Struct>]
+[<StructuredFormatDisplay("{Result}: {Numbers}")>]
+type Equation (result: int64, numbers: int64[]) =
+    member _.Result = result
+    member _.Numbers = numbers
+
+let parse (line:string) =
+    let matches = Regex.Matches(line, "\d+")
+    let result = (Seq.head matches).Value |> int64
+    let numbers = (Seq.tail matches) |> Seq.map (fun x -> x.Value |> int64) |> Array.ofSeq
+    Equation(result, numbers)
+    
+let input =
+    File.ReadAllLines "input.txt"
+    |> Seq.map parse
+
+let testEquationV1 (equation: Equation) =
+    equation.Numbers
+    |> Array.fold (fun current next ->
+        if List.isEmpty current
+        then [ next ]
+        else current |> List.map (fun x -> [ next + x; next * x ]) |> List.concat
+        ) []
+    |> List.tryPick (fun x -> if x = equation.Result then Some equation.Result else None)
+
+input
+|> Seq.map testEquationV1
+|> Seq.choose id
+|> Seq.sum
+|> printfn "Part 1: %d"
+
+let testEquationV2 (equation: Equation) =
+    equation.Numbers
+    |> Array.fold (fun current next ->
+        if List.isEmpty current
+        then [ next ]
+        else current |> List.map (fun x -> [ next + x; next * x; $"{x}{next}" |> int64 ]) |> List.concat
+        ) []
+    |> List.tryPick (fun x -> if x = equation.Result then Some equation.Result else None)
+
+input
+|> Seq.map testEquationV2
+|> Seq.choose id
+|> Seq.sum
+|> printfn "Part 2: %d"
