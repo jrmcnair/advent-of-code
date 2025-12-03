2b16bca78c33fba7296120a458ba51cb1cd001af
Jamie McNair <8563840+jrmcnair@users.noreply.github.com>
2025-12-03 00:31:45 -0500

    Day 3: Part 1 Working

--- 2025/Day3.fs
+++ 2025/Day3.fs
@@ -2,18 +2,23 @@
 
 open System.IO
 
-let parseData (input: string) =
-    ""
-
 let data =
-    "./input/day2.txt"
-    |> File.ReadAllText
-    |> parseData
+    "./input/day3.txt"
+    |> File.ReadLines
 
 module Part1 =
+    let getMaxJoltage (bank: string) : int =
+        let first = bank.Substring(0, bank.Length - 1) |> Seq.max
+        let index = bank.IndexOf(first) + 1
+        let second = bank.Substring(index, bank.Length - index) |> Seq.max
+
+        $"{first}{second}" |> int
+        
     let run () =
-        1
-        |> printfn "Part1: TBD = %d"
+        data
+        |> Seq.map getMaxJoltage
+        |> Seq.sum
+        |> printfn "Part1: Max Joltage = %d"
 
 module Part2 =
     let run () =
--- 2025/tests/AdventOfCode2025.Tests/Day3Tests.fs
+++ 2025/tests/AdventOfCode2025.Tests/Day3Tests.fs
@@ -1,19 +1,33 @@
 ﻿module Tests.Day3
 
+open System
 open AdventOfCode2025.Day3
 open Xunit
 
-let sampleData = """"""
+let sampleData = """987654321111111
+811111111111119
+234234234234278
+818181911112111"""
 
 module Part1 =
     open Part1
-    
+
+    [<Theory>]
+    [<InlineData("987654321111111", 98)>]
+    [<InlineData("811111111111119", 89)>]
+    [<InlineData("234234234234278", 78)>]
+    [<InlineData("818181911112111", 92)>]
+    let ``getMaxJoltage Tests`` (input: string, expected: int) =
+        let actual = getMaxJoltage input
+        Assert.Equal(expected, actual)
+        
     [<Fact>]
     let ``Run Sample Data`` () =
-        let parsedData = parseData sampleData
-        let expected = 0
-
-        let actual = 0
+        let expected = 357
+        let actual = 
+            sampleData.Split(Environment.NewLine)
+            |> Seq.map getMaxJoltage
+            |> Seq.sum
 
         Assert.Equal(expected, actual)
 
@@ -22,9 +36,9 @@ module Part2 =
     
     [<Fact>]
     let ``Run Sample Data`` () =
-        let parsedData = parseData sampleData
-        let expected = 0
+        let data = sampleData.Split('\n')
+        let expected = 4
 
-        let actual = 0
+        let actual = Seq.length data
 
         Assert.Equal(expected, actual)
