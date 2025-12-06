e31e341d4bdc9e351670a73b5ee2bd13a8a391d5
Jamie McNair <8563840+jrmcnair@users.noreply.github.com>
2025-12-06 02:18:27 -0500

    Day 6: part 1 working, started on part 2

---
+++ 2025/Day6.fs
@@ -0,0 +1,88 @@
+﻿module AdventOfCode2025.Day6
+
+open System.IO
+
+type Problem =
+    { Numbers: int64 []
+      Operation: Operation }
+and Operation =
+    | Add
+    | Multiply
+
+module Problem =
+    let solve (problem: Problem) : int64 =
+        match problem.Operation with
+        | Add -> Array.sum problem.Numbers
+        | Multiply -> Array.fold (*) 1L problem.Numbers
+    
+    let toOperation = function
+        | '+' -> Add
+        | '*' -> Multiply
+        | _ -> failwith "Unknown operation"
+
+module Part1 =
+    let parse (size: int) (input: string[]) : Problem array =
+        let numbers =
+            input[0..size-1]
+            |> Array.map (fun (line: string) ->
+                line.Split(" ", System.StringSplitOptions.RemoveEmptyEntries)
+                |> Array.map int64)
+
+        let operations =
+            input[size]
+            |> _.Split(" ", System.StringSplitOptions.RemoveEmptyEntries)
+            |> Array.map (function
+                | "+" -> Add
+                | "*" -> Multiply
+                | _ -> failwith "Unknown operation")
+            
+        [| 0 .. operations.Length - 1 |]
+        |> Array.map (fun col ->
+            { Numbers = Array.init size (fun row -> numbers[row][col])
+              Operation = operations[col] })
+
+    let run () =
+        let problems =
+            "./input/day6.txt"
+            |> File.ReadAllLines
+            |> parse 4
+
+        problems
+        |> Array.map Problem.solve
+        |> Array.sum
+        |> printfn "Part1: %d"
+
+module Part2 =
+    let getNumber (input: string[]) (col: int) =
+        [| 0 .. input.Length - 1 |]
+        |> Array.map (fun row -> input[row][col])
+        |> string
+        |> int64
+
+    let parse (size: int) (input: string[]) : Problem array =
+        let operations = input[size]
+
+        let loop (problems: Problem[]) (col: int) : Problem[] =
+            let operation = operations[col] |> Problem.toOperation
+
+            operations.
+            
+                        // find next operation
+            
+            let x =
+                { Numbers = [|  |]
+                    // [| 0..size-1 |]
+                    // |> Array.map (fun row -> input[row][col])
+                    // |> string
+                    // |> int64
+                  Operation = Problem.toOperation (input[size][col]) }
+            // get operation at idx
+            // starting at idx, get number at each char until we reach next operation or end of line
+            
+            [| x |]
+
+        loop [] 0
+
+    let run () =
+        0
+        |> printfn "Part2: %d"
---
+++ 2025/tests/AdventOfCode2025.Tests/Day6Tests.fs
@@ -0,0 +1,35 @@
+﻿module Tests.Day6
+
+open Xunit
+open AdventOfCode2025.Day6
+
+let sampleData = """123 328  51 64 
+ 45 64  387 23 
+  6 98  215 314
+*   +   *   +  """
+
+module Part1 =
+    open Part1
+    
+    [<Fact>]
+    let ``Run Sample Data`` () =
+        let expected = 4277556L
+        
+        let actual =
+            sampleData.Split(System.Environment.NewLine)
+            |> parse 3
+            |> Array.map Problem.solve
+            |> Array.sum
+        
+        Assert.Equal(expected, actual)
+
+module Part2 =
+    open Part2
+
+    [<Fact>]
+    let ``Run Sample Data`` () =
+        let actual =
+            sampleData.Split(System.Environment.NewLine)
+            |> parse 3
+
+        Assert.Equal(4, actual.Length)
